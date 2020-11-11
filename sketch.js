142312f2d8c65eff55a42d5f7123f5a64ac16a1a
RahulSadasiv <rahulsadasiv@gmail.com>
2020-11-11 20:42:49 +0530

    LOL

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log8
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -23,6 +24,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
+    log8 = new Log(100,100,10,10)
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -58,6 +60,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log8.display();
 
     bird.display();
     platform.display();
